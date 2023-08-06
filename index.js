ecff4009008d0aef8504e301ca22fa149e202201
sokha-web <77844793+sokha-web@users.noreply.github.com>
2023-08-06 21:24:35 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    console.log(count)
+    countEl.textContent = 0
+    count = 0
+}
